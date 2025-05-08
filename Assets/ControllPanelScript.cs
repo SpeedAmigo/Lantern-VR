4cd22512be38f13d6cd7b62ddbf2100aa1b05a50
SpeedAmigo <Kxipi7@gmail.com>
2025-05-08 16:14:06 +0200

    Controller

---
+++ Assets/ControllPanelScript.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ControllPanelScript : SerializedMonoBehaviour
+{
+    [SerializeField] private List<MeshRenderer> lightBulbs;
+    public Dictionary<int, List<GameObject>> switches;
+
+    [SerializeField] Material offMaterial;
+    [SerializeField] Material onMaterial;
+
+    [Button]
+    private void ChangeLight(int value)
+    {
+        var bulbs = switches[value];
+        
+        foreach (var bulb in bulbs)
+        {
+           var renderer = bulb.GetComponent<MeshRenderer>();
+
+           renderer.material = ToggleMaterial(renderer.sharedMaterial);
+        }
+
+        CheckForComplete(lightBulbs);
+    }
+
+    private Material ToggleMaterial(Material material)
+    {
+        if (material == null) return null;
+
+        if (material == offMaterial)
+        {
+            return onMaterial;
+        }
+        else if (material == onMaterial)
+        {
+            return offMaterial;
+        }
+
+        return null;
+    }
+
+    private void CheckForComplete(List<MeshRenderer> bulbsMaterials)
+    {
+        if (bulbsMaterials.All(bulb => bulb.sharedMaterial == onMaterial))
+        {
+            Debug.Log("Running!");
+        }
+    }
+}
